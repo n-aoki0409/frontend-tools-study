a35378a0dc1a1fb3ebb95df9d39795d0dc24deaf
n-aoki0409 <naoki19850409@gmail.com>
2019-12-22 18:50:30 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist/js',
+    filename: 'sample.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { url: false }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  devServer: {
+    contentBase: __dirname + '/public',
+    port: 3000,
+    open: true,
+    historyApiFallback: true
+  }
+};
